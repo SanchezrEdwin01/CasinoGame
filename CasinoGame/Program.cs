4a1e77a177d803ba1836ed7a835feac027317006
SanchezrEdwin01 <sanchez.r.edwin01@gmail.com>
2024-01-18 16:30:11 -0600

    Agregar archivos de proyecto.

---
+++ CasinoGame/Program.cs
@@ -0,0 +1,43 @@
+﻿//Casino Game this is a jack game made in c#
+//author: Edwin Sanchez, email: sanchez797.edwin@gmail.com, github user: SanchezrEdwin01
+
+var tokensAvailables = 0;
+var firstChoiceString = "";
+var firstChoice = 0;
+var whileCorrect = false;
+var wannaContinuePlay = true;
+
+while (wannaContinuePlay)
+{
+    Console.WriteLine("Welcone to 21 game");
+    Console.WriteLine("You´re gonna need some tokens for playing");
+
+    while (!whileCorrect)
+    {
+        Console.WriteLine("Do you wanna buy some tokens? \n Press 1 for going store and buy tickes or press 0 for exit");
+        firstChoiceString = Console.ReadLine();
+
+        if (firstChoiceString == "1" || firstChoiceString == "0")
+        {
+            whileCorrect = true;
+            firstChoice = int.Parse(firstChoiceString);
+        }
+        else
+        {
+            whileCorrect = false;
+            Console.WriteLine("That´s not an option, choose again");
+        }
+    }
+
+    if (firstChoice == 1)
+    {
+
+    }
+    else
+    {
+        wannaContinuePlay = false;
+    }
+
+    //tokensAvailables =
+}
+
